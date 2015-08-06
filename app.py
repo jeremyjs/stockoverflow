0349b176e19feb1c6116f22667f8917557912c31
Jeremy Meyer <jjman505@gmail.com>
2015-08-06 14:24:58 -0400

    updated route to return tabular data for june

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 @app.route("/<symbol>")
 def hello(symbol):
-  datasets = Quandl.search(symbol, authtoken=keys['quandl'])
+  datasets = Quandl.search(symbol, authtoken=keys['quandl'], verbose = False)
   code = datasets[0][u'code']
-  data = Quandl.get(code, authtoken=keys['quandl'], collapse='daily')
+  data = Quandl.get(code, authtoken=keys['quandl'], collapse='daily', trim_start="2015-06-01", trim_end="2015-06-30")
   print data
-  return data.head()
+  return data.to_html()
 
 if __name__ == "__main__":
   app.run(debug=True)
