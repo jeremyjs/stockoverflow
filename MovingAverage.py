9f702bb506ebf63c09af34e93972511e504f76f9
Will Meyer <wwman555@gmail.com>
2015-08-08 00:17:52 -0400

    added moving average //incomplete

---
+++ MovingAverage.py
@@ -0,0 +1,13 @@
+def movingAverage(closing_prices, period=5):
+    seq=closing_prices[0:5]
+    avg1=sum(seq)/5
+    print avg1
+    return [avg1,14,15]
+
+
+def test():
+    L=[11,12,13,14,15,16,17]
+    result=[13,14,15]
+    assert movingAverage(L) == result
+    print "Test Pass"
+test()
