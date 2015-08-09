826192cd6372b0842fb4f296b58ea91d7e86ca9f
Jeremy Meyer <jjman505@gmail.com>
2015-08-08 22:45:57 -0400

    updated signal_crosses test

--- signal_crosses.py
+++ signal_crosses.py
@@ -29,14 +29,16 @@ def signalCrosses(short_moving_averages, long_moving_averages):
     return { "up_crosses": up_crosses, "down_crosses": down_crosses }
 
 def test():
+    message = "signalCrosses"
     s = [8, 10, 12, 14, 13, 10, 7]
     l = [9, 10, 11, 13, 12, 11, 8]
     res = signalCrosses(s, l)
     expected = {'up_crosses': [1], 'down_crosses': [5]}
     if(res == expected):
-        print "All Tests Pass"
+        message = "[....] " + message
     else:
-        print "Got", res, "\n", "Expected", expected
+        message = "[FAIL] " + message + ": Expected " + str(res) + " to equal " + str(expected)
+    print message
 
-if(argv[1] == "test"):
+if(len(argv) > 1 and argv[1] == "test"):
     test()
