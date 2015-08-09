cad97d7ba297f5868c030d38a7cbe99b033c201a
Jeremy Meyer <jjman505@gmail.com>
2015-08-08 20:21:11 -0400

    added a layout file

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from flask import render_template
 from config import keys
 import Quandl
 
-app = Flask(__name__)
-@app.route("/")
+app = Flask(__name__.split('.')[0])
+@app.route('/')
 def root():
     return render_template('landing.html')
 
-@app.route("/<symbol>")
+@app.route('/<symbol>')
 def hello(symbol):
     datasets = Quandl.search(symbol, authtoken=keys['quandl'], verbose = False)
     code = datasets[0][u'code']
-    data = Quandl.get(code, authtoken=keys['quandl'], collapse='daily', trim_start="2015-06-01", trim_end="2015-06-30")
+    data = Quandl.get(code, authtoken=keys['quandl'], collapse='daily', trim_start='2015-06-01', trim_end='2015-06-30')
     print data
     return data.to_html()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
