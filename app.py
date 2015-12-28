04db62460f19ce063b09b1de6ac933c707164d9a
Jeremy Meyer <jjman505@gmail.com>
2015-12-28 13:12:44 -0500

    feat: add start_date, end_date query params to stock query route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 from config import keys
 import Quandl
 
@@ -10,9 +11,14 @@ def root():
 
 @app.route('/<symbol>')
 def hello(symbol):
+    query_params = request.args
+    print (query_params)
+    trim_start = query_params.get('start_date') or '2015-12-01'
+    trim_end = query_params.get('end_date') or '2015-12-31'
+    print (trim_end, trim_start)
     datasets = Quandl.search(symbol, authtoken=keys['quandl'], verbose = False)
     code = datasets[0][u'code']
-    data = Quandl.get(code, authtoken=keys['quandl'], collapse='daily', trim_start='2015-06-01', trim_end='2015-06-30')
+    data = Quandl.get(code, authtoken=keys['quandl'], collapse='daily', trim_start=trim_start, trim_end=trim_end)
     return data.to_html()
 
 if __name__ == '__main__':
