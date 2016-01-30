857a5aae9e0c218dbde2fa6caf21af9f286fe9d6
Jeremy Meyer <jjman505@gmail.com>
2016-01-30 14:24:10 -0500

    update simulation route in app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import Quandl
 import json
 
 from simulate import simulate
+from get_prices import get_prices
 
 app = Flask(__name__.split('.')[0])
 @app.route('/')
@@ -13,20 +14,19 @@ def root():
     return render_template('landing.html')
 
 @app.route('/simulate/<symbol>')
-def graph(symbol):
+def run_simulation(symbol):
     query_params = request.args
-    print (query_params)
-    trim_start = query_params.get('start_date') or '2015-12-01'
+    trim_start = query_params.get('start_date') or '2015-11-01'
     trim_end = query_params.get('end_date') or '2015-12-31'
-    datasets = Quandl.search(symbol, authtoken=keys['quandl'], verbose = False)
-    code = datasets[0][u'code']
-    input_data = Quandl.get(code, authtoken=keys['quandl'], collapse='daily', trim_start=trim_start, trim_end=trim_end)
-    input_data = input_data.to_json()
-    input_data = json.loads(input_data)
-    close_prices = input_data['Close']
-    close_prices = close_prices.values()
-    data = simulate(close_prices)
-    return json.dumps(data)
+    prices = get_prices([symbol], trim_start=trim_start, trim_end=trim_end)
+    prices = prices[symbol]
+    signal_crosses, earnings = simulate(prices)
+    dailies = prices
+    for timestamp in dailies.keys():
+        dailies[timestamp] = { 'price': prices[timestamp], 'signal': signal_crosses[timestamp] }
+    signal_crosses = [ [k, v] for k, v in signal_crosses.iteritems()]
+    prices = { s: [ [k, v] for k, v in prices[s].iteritems()] for s in prices.keys() }
+    return json.dumps({'earnings': earnings, 'dailies': dailies, 'prices': prices, 'signal_crosses': signal_crosses})
 
 @app.route('/<symbol>')
 def hello(symbol):
