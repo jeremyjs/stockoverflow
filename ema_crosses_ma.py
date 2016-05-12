a43de37b294c5a212e1470c410b6df4a7b5be3ef
billiam <wwman555@gmail.com>
2016-05-12 15:52:34 -0400

    add curr_signal func in ema_crosses_ma

--- ema_crosses_ma.py
+++ ema_crosses_ma.py
@@ -1,4 +1,5 @@
 from exponential_moving_average import ewmas
+from moving_average import moving_averages
 from signal_crosses import signal_crosses
 from sys import argv
 from math import floor
@@ -20,26 +21,10 @@ def ema_ma_signal_crosses(prices, ema_period=DEFAULT_EMA_PERIOD, ma_period=False
 
     return signal_crosses(ema, ma)
 
-def lssc_interval(prices, interval):
-    interval_multipliers = {
-        'DAYS': 1,
-        'HOURS': 24,
-        'MINUTES': 24*60,
-        'SECONDS': 24*60*60
-    }
-    if isinstance(interval, str): multiplier = interval_multipliers[interval]
-    elif isinstance(interval, (int, float)): multiplier = interval
-    new_ema_period = DEFAULT_EMA_PERIOD * multiplier
-    new_ma_period = DEFAULT_MA_PERIOD * multiplier
-    return long_short_signal_crosses(prices, ema_period=new_ema_period, ma_period=new_ema_period)
-
-def lssc_hourly(prices):
-    return lssc_interval(prices, 'HOURS')
-
 def test():
     message = "long_short_signal_crosses"
     cp = [22.27,22.19,22.08,22.17,22.18,22.13,22.23,22.43,22.24,22.29,22.15,22.39,22.18,22.13,22.19,22.08,22.17,22.23,22.43]
-    res = long_short_signal_crosses(cp, ema_period=3, ema_period=7)
+    res = ema_ma_signal_crosses(cp, ema_period=5, ma_period=5)
     expected = {'up_crosses': [5, 11], 'down_crosses': [4, 6]}
 
     if(res == expected):
@@ -48,6 +33,32 @@ def test():
         message = "[FAIL] " + message + ": Expected " + str(res) + " to equal " + str(expected)
 
     print message
+
+def curr_signal(prices):
+    message = "curr_signal"
+    currprices = list[-5:]
+    sma = (sum(currprices)/5)
+    ema = ewmas(prices, 5)
+    swap = false 
+    i=1
+    trend_val = ema-sma
+    while(!swap || i < len(prices)-5)
+        currprices = prices[-5-i:-i]
+        sma = (sum(currprices)/5)
+        ema = ewmas(prices, 5)
+
+        if (trend_val < ema-sma)
+            swap = true
+            return -1
+        if (trend_val > ema-sma)
+            swap = true
+            return 1
+        i+=1
+        count+=1
+        
+
+
+    return('up_crosses', 'down_crosses')
 
 if(len(argv) > 1 and argv[1] == "test"):
     test()
