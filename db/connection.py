83626caa10d6639ec561a47861a1f190db00c37d
Jeremy Meyer <jjman505@gmail.com>
2016-05-13 06:31:00 -0400

    setup db connection to truefx

--- db/connection.py
+++ db/connection.py
@@ -2,3 +2,7 @@ import pymongo
 
 client = pymongo.MongoClient('mongodb://root:rootpass@ds033103.mongolab.com:33103')
 db = client['stock-overflow']
+
+truefx_client = pymongo.MongoClient('mongodb://localhost')
+truefx = truefx_client['truefx']['ticks']
+
